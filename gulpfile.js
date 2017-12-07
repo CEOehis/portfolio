f2cc759ab7b7918b54d96f11248e22eed5be9bfe
ceoehis <ceo.ehis@outlook.com>
2017-12-07 20:31:47 +0100

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require('gulp');
+const connect = require('gulp-connect');
+const sass = require('gulp-sass');
+
+gulp.task('server', function() {
+  return connect.server({
+    port: 9999,
+    livereload: true
+  });
+});
+
+gulp.task('reload', function () {
+  return gulp.src('./index.html').pipe(connect.reload());
+});
+
+
+gulp.task('styles', function() {
+	gulp.src('scss/**/*.scss')
+					.pipe(sass().on('error', sass.logError))
+					.pipe(gulp.dest('./css/'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['./index.html', 'css/*.css', 'js/*.js', 'scss/*.scss'], ['reload', 'styles']);
+});
+
+gulp.task('default', ['server', 'watch', 'styles']);
+
+
